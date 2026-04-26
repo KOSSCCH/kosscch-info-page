067a0c379574536a91c4345f8b9289f7628f85ca
민제 <minjae5474@naver.com>
2026-04-26 17:47:59 +0900

    Chore: Github Actions를 이용한 자동 배포 환경 구축 완료

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://KOSSCCH.github.io",
-  base: "/Sandbox",
+  base: "/kosscch-info-page",
 });
